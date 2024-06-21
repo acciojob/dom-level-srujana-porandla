69b94335cdeda2e90a2c48e81a3207ea4b2ed288
srujana-porandla <131435996+srujana-porandla@users.noreply.github.com>
2024-06-21 18:05:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+window.onload = function() {
+    let element = document.getElementById('level');
+
+    let levelCount = 0;
+
+    while (element) {
+        levelCount++;
+        element = element.parentElement;
+    }
+
+    alert(`The level of the element is: ${levelCount}`);
+};
